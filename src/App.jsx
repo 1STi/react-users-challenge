fab0fd6bf5735d7428291efde466ab532e967ef4
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-05-18 16:59:04 -0300

    get users infos ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import './App.css';
 import api from './services/api';
+import { useState, useEffect } from 'react';
 
 function App() {
-  api.get('/').then((res) => {
-    const { results } = res.data;
-    console.log(results);
-  });
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    api.get('/').then((res) => {
+      const { results } = res.data;
+      setUsers(results);
+    });
+  }, []);
 
   return (
     <>
-      <h1>h1testando</h1>
+      {console.log(users)}
+      <h1>h1PaginaDeTesteee</h1>
+      <div className="users">
+        {users.map((user, login) => {
+          return (
+            <div key={login.uuid}>
+              <img src={user.picture.thumbnail} /> &nbsp;
+              {user.name.first} &nbsp;
+              {user.name.last} &nbsp;
+              {user.dob.age} &nbsp;
+              {user.location.country} &nbsp;
+              {user.gender}
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
